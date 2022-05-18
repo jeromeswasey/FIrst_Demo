c24dc9fcedcc8eb8a7427000d7ead06e2faf9e4f
jeromeswasey <jeromeswasey@gmail.com>
2022-05-18 17:35:40 -0400

    first

---
+++ SysModule.py
@@ -0,0 +1,9 @@
+price = 10 #integer variable
+rating = 4 #float variable
+name = "Mosh" #string variable
+is_published = True #boolean variable
+name = input ("what is your name? {Enter Name} " )
+birth_year = int(input( "Birth year: "))
+print ('hi ' + name)
+print (name)
+
