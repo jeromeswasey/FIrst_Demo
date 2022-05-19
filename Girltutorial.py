347ecf8a4263ab5ad746c50c0619d1c5da0265e9
jeromeswasey <jeromeswasey@gmail.com>
2022-05-19 11:51:02 -0400

    master

--- Girltutorial.py
+++ Girltutorial.py
@@ -3,9 +3,7 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days): #function
-	print (f"20 days are {20 * calculation_to_units} {name_of_unit}")
+	print (f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
 	print ("All good!")
 
-
-
-days_to_units(35) #Calltofunction
+days_to_units(20)
--- days_to_hours_converter.py
+++ days_to_hours_converter.py
@@ -3,9 +3,12 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days): #function
-	print (f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
-	print ("All good!")
+	return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
+	
+
+user_input = input("Hey user, enter a number of days and I will conver it to hours!\n")
+user_input = int(user_input) #converts string to number (Casting called) int is built in python function
+calculated_value = days_to_units(user_input)
+print(calculated_value)
 
 
-
-days_to_units(35) #Calltofunction
