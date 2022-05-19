11d03982a30e89b3fc34e37a79d3cf5e91aefe7b
jeromeswasey <jeromeswasey@gmail.com>
2022-05-19 10:57:31 -0400

    master

---
+++ days_to_hours_converter.py
@@ -0,0 +1,11 @@
+calculation_to_units = 24 #variables
+name_of_unit = "hours" 
+
+
+def days_to_units(num_of_days): #function
+	print (f"20 days are {20 * calculation_to_units} {name_of_unit}")
+	print ("All good!")
+
+
+
+days_to_units(35) #Calltofunction
